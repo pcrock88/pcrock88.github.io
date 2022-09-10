cb1582201fceac3ec243eb1a15ed53616b52913e
pcrock <pcrock@foxmail.com>
2022-09-10 15:23:48 +0800

    Update pcrock.js

--- js/pcrock.js
+++ js/pcrock.js
@@ -14,7 +14,7 @@ function show(d1){if(document.getElementById(d1).style.display=='none'){document
 var lang=new Array();var userAgent=navigator.userAgent.toLowerCase();var is_opera=userAgent.indexOf("opera")!=-1&&opera.version();var is_moz=(navigator.product=="Gecko")&&userAgent.substr(userAgent.indexOf("firefox")+8,3);var is_ie=(userAgent.indexOf("msie")!=-1&&!is_opera)&&userAgent.substr(userAgent.indexOf("msie")+5,3);Array.prototype.push=function(value){this[this.length]=value;return this.length};function $(objname){return document.getElementById(objname)}function runCode(objname){var winname=window.open("","_blank","");var obj=$(objname);winname.document.open("text/html","replace");winname.opener=null;winname.document.write(obj.value);winname.document.close()}function saveCode(objname){var winname=window.open("","_blank","top=10000");winname.document.open("text/html","replace");var obj=$(objname);winname.document.write(obj.value);winname.document.execCommand("saveas","","code.htm");winname.close()}; //VIP
 var data = [
 {name:"OKjx",url:"https://okjx.cc/?url="},
-{name:"OKjx",url:"https://z1.m1907.cn/?jx="},
+{name:"1907",url:"https://z1.m1907.cn/?jx="},
 {name:"BDyun",url:"https://jx.jxbdzyw.com/m3u8/?url="},
 {name:"660ejx",url:"https://660e.com/?url="},
 {name:"17jx",url:"https://www.1717yun.com/jiexi/?url="},
